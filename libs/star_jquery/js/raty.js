a25f5d003eeac8f3404a800f6d932789e44472e0
Vasil-Dlutski <47484501+WebDVV@users.noreply.github.com>
2022-09-08 12:11:31 +0300

    Add files via upload

---
+++ libs/star_jquery/js/raty.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function(){
+	$('.rate1').raty({
+		click: function(score, evt) {
+		alert('ID: ' + this.id + "\nscore: " + score);
+	 }
+	});
+	
+	$('.rate2').raty({
+		number : 10,
+		starOff : 'img/rating_passiv.png',
+		starOn  : 'img/rating_activ.png'
+	});
+	
+	$('.rate3').raty({
+		number : 4,
+		target : '.name',
+		hints  : ['очень плохо', 'плохо', 'нормально', 'хорошо', 'очень хорошо'], 
+	});
+});
